691a273fef534308299a30401861f93ccbed6f24
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-19 00:27:08 +0530

    pushing all

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 
 chmod 400 pari-test.pem
-rsync -rave "ssh -i pari-test.pem" ./pari ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
+rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
