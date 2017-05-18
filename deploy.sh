d047e486864cd4a7416d42f3b8e100e520cbb349
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-19 00:17:57 +0530

    changing pem

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-rsync -rave "ssh -i travis.pem" ./pari ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
+rsync -rave "ssh -i pari-test.pem" ./pari ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
