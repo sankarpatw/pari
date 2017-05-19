44b2bf596e6adfa5703b1ed29f7ab9c739320a95
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-19 11:03:59 +0530

    Adding deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!bin/bash
 
-chmod 400 pari-test.pem
-rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
+#chmod 400 pari-test.pem
+#rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
+git clone git@github.com:PARINetwork/pari-ansible.git
