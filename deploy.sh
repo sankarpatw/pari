98ac45b34c90ce0552f50f2dabffd313271e23fe
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-19 13:41:52 +0530

    adding verbose run

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cp pari-test.pem ~/.ssh/id_rsa
 #rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
 git clone https://github.com/sankarpatw/pari-ansible.git
 cd pari-ansible
-ansible-playbook -l staging -u ubuntu  -i hosts.yml site.yml -vvvv
+ansible-playbook -l staging -u ubuntu  -i hosts.yml site.yml
