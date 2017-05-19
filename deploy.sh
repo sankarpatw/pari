3f5984466796a22439c38eaa1677df3d745b0ab4
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-19 13:20:11 +0530

    adding verbose run

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
 chmod 400 pari-test.pem
+cp pari-test.pem ~/.ssh/id_rsa
 #rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
 git clone https://github.com/sankarpatw/pari-ansible.git
 cd pari-ansible
