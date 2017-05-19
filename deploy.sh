664e4fa9041e6f24167fcfbe91731c09b37b38ef
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-19 11:30:16 +0530

    Adding master for deployment

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 #chmod 400 pari-test.pem
 #rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
-git clone git@github.com:PARINetwork/pari-ansible.git
+git clone https://github.com/sankarpatw/pari-ansible.git
