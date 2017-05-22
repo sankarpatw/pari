cf8efba471392cb024011ca5876b4aa05fcb9b0b
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-22 13:56:02 +0530

    changing the permissions for rsa

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 #chmod 400 deploy_rsa
 #todo find a elegant way to instruct ansible to take right pem
 cp deploy_rsa ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
 #rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
 git clone https://github.com/sankarpatw/pari-ansible.git
 cd pari-ansible
