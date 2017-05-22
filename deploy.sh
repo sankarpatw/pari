d217144cc973d41fcfa81f45720ab74e9fbb3eab
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-22 16:38:32 +0530

    Adding Deployment File

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@
 chmod 400 pari-test.pem
 #todo find a elegant way to instruct ansible to take right pem
 cp pari-test.pem ~/.ssh/id_rsa
-#chmod 400 ~/.ssh/id_rsa
 #rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
 git clone https://github.com/sankarpatw/pari-ansible.git
 cd pari-ansible
-ansible-playbook -l staging -u ubuntu -i hosts.yml -t deploy site.yml -vvvv
+ansible-playbook -l staging -u ubuntu -i hosts.yml -t deploy site.yml
