5f054a19cb157b3d6ffff28827c571910b25f565
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-22 13:21:02 +0530

    adding verbose logging

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ cp deploy_rsa ~/.ssh/id_rsa
 #rsync -rave "ssh -i pari-test.pem" .* ubuntu@ec2-52-14-252-14.us-east-2.compute.amazonaws.com:/home/ubuntu/pari/
 git clone https://github.com/sankarpatw/pari-ansible.git
 cd pari-ansible
-ansible-playbook -l staging -u ubuntu -i hosts.yml -t deploy site.yml
+ansible-playbook -l staging -u ubuntu -i hosts.yml -t deploy site.yml -vvvv
